c13263f7856cb8f0805914b18d6a226d0771a10a
juttada sai kumar <likkisai007@gmail.com>
2026-02-13 13:01:49 +0530

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.post(
 
 app.use(cors(
     {
-        origin: ["http://localhost:5173", "http://localhost:5174"],
+        origin: ["https://examnotesaifrontend.onrender.com"],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
